bf1475337c1c66028b828278c02efe18818be180
Anton Katsuba <antonikk7@gmail.com>
2021-05-27 19:19:03 +0300

    Drawing pictures with wx.PaintDC instead of naive bitmap

--- src/ui.py
+++ src/ui.py
@@ -1,38 +1,36 @@
 import wx, cv2
 import numpy as np
 
-image_data = None
-image_path = None
 
-class TestFrame(wx.Frame):
+class AppFrame(wx.Frame):
 
 
     def __init__(self, parent, title):
         super().__init__(parent, title=title, size = (1400,900))
 
-        image_data = None
-        image_path = None
-
         self.initialize_GUI()
     
+
     def initialize_GUI(self):
         self.sp = wx.SplitterWindow(self, style = wx.SP_3DBORDER)
         self.p1 = PanelWithButtons(self.sp)
         self.p2 = PanelWithImage(self.sp)
         self.sp.SplitVertically(self.p1, self.p2, 300)
 
+
         menubar = wx.MenuBar()
         filemenu = wx.Menu()
 
-        filequit = filemenu.Append(wx.ID_EXIT, "Quit", "Quit application")
         fileopen = filemenu.Append(wx.ID_FILE, "Open", "Open an image")
+        filequit = filemenu.Append(wx.ID_EXIT, "Quit", "Quit application")
 
         menubar.Append(filemenu, "&File")
 
         self.SetMenuBar(menubar)
 
-        self.Bind(wx.EVT_MENU, self.OnQuit, filequit)
         self.Bind(wx.EVT_MENU, self.OnBrowse, fileopen)
+        self.Bind(wx.EVT_MENU, self.OnQuit, filequit)
+
 
         self.Centre()
 
@@ -48,31 +46,42 @@ class TestFrame(wx.Frame):
                 self.load_file()
 
     def load_file(self):
-        print(self.image_path)
         img = cv2.imread(self.image_path[0])
         img = cv2.cvtColor(np.uint8(img), cv2.COLOR_BGR2RGB) 
 
         h, w = img.shape[:2]
 
-        wxbmp = wx.Bitmap.FromBuffer(w, h, img)
-        self.p2.bmp = wxbmp
-        
+        self.p2.bitmap = wx.Bitmap.FromBuffer(w, h, img)
+
 
 class PanelWithButtons(wx.Panel):
-    def __init__(self, parent):
+    def __init__(self, parent): 
         super().__init__(parent)
 
-        button = wx.Button(self, label = "Help me")
+        wx.Button(self, label = "Help me")
 
         self.SetBackgroundColour((225, 225, 225))
+
 
 class PanelWithImage(wx.Panel):
+
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.bitmap = None
+
+        wx.EVT_PAINT(self, self.on_paint)
+
         self.SetBackgroundColour((225, 225, 225))
+
+
+    def on_paint(self, e):
+        canvas = wx.PaintDC(self)
+        canvas.DrawBitmap(self.bitmap, 30, 20)
+
 
 
 app = wx.App()
-frame = TestFrame(None, "Decryptococcus")
+frame = AppFrame(None, "Decryptococcus")
 frame.Show()
 app.MainLoop()
