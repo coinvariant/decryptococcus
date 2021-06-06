c7b7ac7038771f9e4ce4a45c91d3783b3db2f709
Anton Katsuba <antonikk7@gmail.com>
2021-06-06 11:29:44 +0300

    More buttons, slider

--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,6 @@
 import wx, cv2
 import numpy as np
-from wx.core import VERTICAL
+from wx.core import OR_INVERT, VERTICAL
 
 
 class AppFrame(wx.Frame):
@@ -52,6 +52,8 @@ class AppFrame(wx.Frame):
 
         h, w = img.shape[:2]
 
+        # TODO: such reference needs to be dealt with
+
         self.p2.bitmap = wx.Bitmap.FromBuffer(w, h, img)
 
 
@@ -60,20 +62,57 @@ class PanelWithButtons(wx.Panel):
         super().__init__(parent)
 
         vbox = wx.BoxSizer(orient=wx.VERTICAL)
+
         b1 = wx.Button(self, label = "Geometric filter")
+        s1 = wx.Slider(self, style = wx.SL_AUTOTICKS)   
         b2 = wx.Button(self, label = "Erosion")
         b3 = wx.Button(self, label = "Dilation")
         b4 = wx.Button(self, label = "Averaging")
+        b5 = wx.Button(self, label = "Gamma")
+        b6 = wx.Button(self, label = "Logarithmic")
 
-        vbox.Add(b1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)  
+        vbox.Add(b1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(s1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)    
         vbox.Add(b2, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)   
         vbox.Add(b3, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)   
-        vbox.Add(b4, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)         
-
-
+        vbox.Add(b4, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(b5, wx.ID_ANY, wx.EXPAND | wx.ALL, 20) 
+        vbox.Add(b6, wx.ID_ANY, wx.EXPAND | wx.ALL, 20) 
+             
         self.SetSizer(vbox)
 
+        s1.SetTickFreq(5)
+        print(s1.GetTickFreq())
+
+        self.Bind(wx.EVT_BUTTON, self.OnGeometricPress, b1)
+        self.Bind(wx.EVT_BUTTON, self.OnErosionPress, b2)
+        self.Bind(wx.EVT_BUTTON, self.OnDilationPress, b3)
+        self.Bind(wx.EVT_BUTTON, self.OnAveragePress, b4)
+        self.Bind(wx.EVT_BUTTON, self.OnGammaPress, b5)
+        self.Bind(wx.EVT_BUTTON, self.OnLogarithmicPress, b6)
+
+
         self.SetBackgroundColour((225, 225, 225))
+    
+    def OnErosionPress(self, e):
+        print("Erosion")    
+    
+    def OnDilationPress(self, e):
+        print("Dilation")
+
+    def OnGeometricPress(self, e):
+        print("Geometric")
+
+    def OnAveragePress(self, e):
+        print("Average")
+
+    def OnGammaPress(self, e):
+        print("Gamma")
+
+    def OnLogarithmicPress(self, e):
+        print("Logarithmic")
+
+    
 
 
 class PanelWithImage(wx.Panel):
