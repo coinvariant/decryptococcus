2a484d354605faa5ecad79341c928f2b97600351
Anton Katsuba <antonikk7@gmail.com>
2021-06-06 13:24:58 +0300

    averaging filter added

--- src/core.py
+++ src/core.py
@@ -1,2 +1,17 @@
 import cv2
 import numpy as np
+
+def apply_averaging_spatial_filter(image, kernel_size):
+    return cv2.blur(image, kernel_size)
+
+def apply_geometric_spatial_filter(image, *args):
+    pass
+
+def apply_erosion(image, *args):
+    pass
+
+def apply_dilation(image, *args):
+    pass
+
+def run_automatic_segmentation(image):
+    pass
--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,8 @@
-import wx, cv2
+import wx
+import cv2
 import numpy as np
-from wx.core import OR_INVERT, VERTICAL
+import core
+
 
 IMAGE = None
 IMAGE_BUFFER = None
@@ -8,19 +10,16 @@ IMAGE_BUFFER = None
 
 class AppFrame(wx.Frame):
 
-
     def __init__(self, parent, title):
-        super().__init__(parent, title=title, size = (1400,900))
+        super().__init__(parent, title=title, size=(1400, 900))
 
         self.initialize_GUI()
-    
 
     def initialize_GUI(self):
-        self.sp = wx.SplitterWindow(self, style = wx.SP_3DBORDER)
+        self.sp = wx.SplitterWindow(self, style=wx.SP_3DBORDER)
         self.p1 = PanelWithButtons(self.sp)
         self.p2 = PanelWithImage(self.sp)
         self.sp.SplitVertically(self.p1, self.p2, 300)
-
 
         menubar = wx.MenuBar()
         filemenu = wx.Menu()
@@ -35,9 +34,7 @@ class AppFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnBrowse, fileopen)
         self.Bind(wx.EVT_MENU, self.OnQuit, filequit)
 
-
         self.Centre()
-
 
     def OnQuit(self, e):
         self.Close()
@@ -54,31 +51,32 @@ class AppFrame(wx.Frame):
 
         img = cv2.imread(self.image_path[0])
         IMAGE = cv2.cvtColor(np.uint8(img), cv2.COLOR_BGR2RGB)
-        IMAGE_BUFFER = IMAGE.copy() 
+        IMAGE_BUFFER = IMAGE.copy()
 
 
 class PanelWithButtons(wx.Panel):
-    def __init__(self, parent): 
+
+    def __init__(self, parent):
         super().__init__(parent)
 
         vbox = wx.BoxSizer(orient=wx.VERTICAL)
 
-        b1 = wx.Button(self, label = "Geometric filter")
-        s1 = wx.Slider(self, style = wx.SL_AUTOTICKS)   
-        b2 = wx.Button(self, label = "Erosion")
-        b3 = wx.Button(self, label = "Dilation")
-        b4 = wx.Button(self, label = "Averaging")
-        b5 = wx.Button(self, label = "Gamma")
-        b6 = wx.Button(self, label = "Logarithmic")
+        b1 = wx.Button(self, label="Geometric filter")
+        s1 = wx.Slider(self, style=wx.SL_AUTOTICKS)
+        b2 = wx.Button(self, label="Erosion")
+        b3 = wx.Button(self, label="Dilation")
+        b4 = wx.Button(self, label="Averaging")
+        b5 = wx.Button(self, label="Gamma")
+        b6 = wx.Button(self, label="Logarithmic")
 
         vbox.Add(b1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
-        vbox.Add(s1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)    
-        vbox.Add(b2, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)   
-        vbox.Add(b3, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)   
+        vbox.Add(s1, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(b2, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(b3, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
         vbox.Add(b4, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
-        vbox.Add(b5, wx.ID_ANY, wx.EXPAND | wx.ALL, 20) 
-        vbox.Add(b6, wx.ID_ANY, wx.EXPAND | wx.ALL, 20) 
-             
+        vbox.Add(b5, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(b6, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+
         self.SetSizer(vbox)
 
         s1.SetTickFreq(5)
@@ -91,12 +89,11 @@ class PanelWithButtons(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.OnGammaPress, b5)
         self.Bind(wx.EVT_BUTTON, self.OnLogarithmicPress, b6)
 
-
         self.SetBackgroundColour((225, 225, 225))
-    
+
     def OnErosionPress(self, e):
-        print("Erosion")    
-    
+        print("Erosion")
+
     def OnDilationPress(self, e):
         print("Dilation")
 
@@ -106,13 +103,16 @@ class PanelWithButtons(wx.Panel):
     def OnAveragePress(self, e):
         print("Average")
 
+        global IMAGE_BUFFER
+        IMAGE_BUFFER = core.apply_averaging_spatial_filter(
+            IMAGE_BUFFER, (4, 4))
+        frame.p2.Refresh()
+
     def OnGammaPress(self, e):
         print("Gamma")
 
     def OnLogarithmicPress(self, e):
         print("Logarithmic")
-
-    
 
 
 class PanelWithImage(wx.Panel):
@@ -124,7 +124,6 @@ class PanelWithImage(wx.Panel):
 
         self.SetBackgroundColour((225, 225, 225))
 
-
     def on_paint(self, e):
         canvas = wx.PaintDC(self)
         h, w = IMAGE_BUFFER.shape[:2]
@@ -132,8 +131,8 @@ class PanelWithImage(wx.Panel):
         canvas.DrawBitmap(bmp, 30, 20)
 
 
-
-app = wx.App()
-frame = AppFrame(None, "Decryptococcus")
-frame.Show()
-app.MainLoop()
+if __name__ == "__main__":
+    app = wx.App()
+    frame = AppFrame(None, "Decryptococcus")
+    frame.Show()
+    app.MainLoop()
