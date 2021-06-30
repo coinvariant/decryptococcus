a5261b35d48256e86933f51a7641fc86d073e0a6
Anton Katsuba <antonikk7@gmail.com>
2021-06-30 14:50:05 +0300

    components

--- src/core.py
+++ src/core.py
@@ -26,45 +26,57 @@ def apply_gamma_transform(image, c, y):
     out *= 255
     return out.astype('uint8')
 
+def eliminate_blue_objects(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    res = cv2.inRange(image, (90, 0, 0), (110, 255, 255))
+    replacement = np.mean(image, axis=(0,1))
+    print(replacement)
+
+    h = res // 255 * replacement[0].astype("uint8")
+    s = res // 255 * replacement[1].astype("uint8")
+    v = res // 255 * replacement[2].astype("uint8")
+
+    print(h.dtype)
+
+    image = cv2.bitwise_and(image, image, mask = ~res)
+
+    image[:, :, 0] += h
+    image[:, :, 1] += s
+    image[:, :, 2] += v
+
+    image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    return image
+
 
 def detect_edges(image):
     cv2.imshow("coleor", image)
+    bl = eliminate_blue_objects(image)
+    cv2.imshow('blue', bl)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow("1", image)
-    image = cv2.bilateralFilter(image, 9, 75, 75)
-    # image = cv2.GaussianBlur(image, (3,3), 1)
+    # image = cv2.bilateralFilter(image, 9, 75, 75)
+    image = cv2.GaussianBlur(image, (3,3), 1)
     cv2.imshow("2", image)
     edges = cv2.Laplacian(image, -1, ksize=3, scale=1)
     cv2.imshow("edge", edges)
     _, mask = cv2.threshold(edges, 25, 255, cv2.THRESH_BINARY)
     cv2.imshow("mask", mask)
     cv2.waitKey(0)
+    return mask
 
-def exper(image):
-    Z = image.copy()
-    Z = Z.reshape((-1,3))
-    # convert to np.float32
-    Z = np.float32(Z)
-    # define criteria, number of clusters(K) and apply kmeans()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 3
-    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-    # Now convert back into uint8, and make original image
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    res2 = res.reshape((image.shape))
-    cv2.imshow('res2',res2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def analyze_connected_components(image):
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(image)
+
 
 def run_automatic_segmentation(image):
-    pass
+    components = detect_edges(image)
+    data = analyze_connected_components(components)
 
 
 if __name__ == "__main__":
-    test1 = cv2.imread("./test/test1.jpg")
-    test2 = cv2.imread("./test/test2.jpg")
-    test3 = cv2.imread("./test/test3.jpg")
+    test1 = cv2.cvtColor(cv2.imread("./test/test1.jpg"), cv2.COLOR_BGR2RGB)
+    test2 = cv2.cvtColor(cv2.imread("./test/test2.jpg"), cv2.COLOR_BGR2RGB)
+    test3 = cv2.cvtColor(cv2.imread("./test/test3.jpg"), cv2.COLOR_BGR2RGB)
 
     detect_edges(test2)
     # exper(test1)    
